171bf8176e6462091449462414ffca5e5e084700
MathGaron <mathieugaron91@gmail.com>
2017-08-24 19:51:13 -0400

    add helper class to keep track of detection results

---
+++ ap_accumulator.py
@@ -0,0 +1,45 @@
+"""
+    Simple accumulator class that keeps track of True positive, False positive and False negative
+    to compute precision and recall of a certain class
+"""
+
+
+class APAccumulator:
+    def __init__(self):
+        self.TP, self.FP, self.FN = 0, 0, 0
+
+    def inc_good_prediction(self):
+        self.TP += 1
+
+    def inc_bad_prediction(self):
+        self.FP += 1
+
+    def inc_not_predicted(self):
+        self.FN += 1
+
+    @property
+    def precision(self):
+        total_predicted = self.TP + self.FP
+        if total_predicted == 0:
+            total_gt = self.TP + self.FN
+            if total_gt == 0:
+                return 1
+            else:
+                return 0
+        return self.TP / total_predicted
+
+    @property
+    def recall(self):
+        total_gt = self.TP + self.FN
+        if total_gt == 0:
+            return 1
+        return self.TP / total_gt
+
+    def __str__(self):
+        str = ""
+        str += "True positives : {}\n".format(self.TP)
+        str += "False positives : {}\n".format(self.FP)
+        str += "False Negatives : {}\n".format(self.FN)
+        str += "Precision : {}\n".format(self.precision)
+        str += "Recall : {}\n".format(self.recall)
+        return str
