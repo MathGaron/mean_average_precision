a46d26826e3e303ee71ad4b86c7c9f02e081132c
MathGaron <mathieugaron91@gmail.com>
2017-08-24 20:11:29 -0400

    fix setup

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 setup(
     name='mean_average_precision',
     version='0.1',
-    packages=[''],
+    packages=['mean_average_precision'],
     url='',
     license='MIT',
     author='Mathieu Garon',
